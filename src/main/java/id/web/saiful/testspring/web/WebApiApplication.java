4c7219c2abc5f6c93095a15156ff19f73eba0286
Muhammad Saiful Islam <muhammad@saiful.web.id>
2017-06-08 14:44:59 +0700

    Create Spring project

---
+++ src/main/java/id/web/saiful/testspring/web/WebApiApplication.java
@@ -0,0 +1,12 @@
+package id.web.saiful.testspring.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebApiApplication.class, args);
+	}
+}
