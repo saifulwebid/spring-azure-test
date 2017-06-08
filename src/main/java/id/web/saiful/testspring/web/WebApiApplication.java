cff91f0be861dbca43b3317c47e693eb21a16c2e
Muhammad Saiful Islam <muhammad@saiful.web.id>
2017-06-08 16:30:31 +0700

    Unextend SpringBootServletInitializer

--- src/main/java/id/web/saiful/testspring/web/WebApiApplication.java
+++ src/main/java/id/web/saiful/testspring/web/WebApiApplication.java
@@ -2,16 +2,9 @@ package id.web.saiful.testspring.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class WebApiApplication extends SpringBootServletInitializer {
-	
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(WebApiApplication.class);
-	}
+public class WebApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WebApiApplication.class, args);
