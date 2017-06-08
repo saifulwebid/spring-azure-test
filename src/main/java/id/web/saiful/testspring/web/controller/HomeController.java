aec0cee6de58bec90e34f030386402a6543541f4
Muhammad Saiful Islam <muhammad@saiful.web.id>
2017-06-08 14:58:18 +0700

    Add HomeController

---
+++ src/main/java/id/web/saiful/testspring/web/controller/HomeController.java
@@ -0,0 +1,14 @@
+package id.web.saiful.testspring.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+	@RequestMapping("")
+	public String index() {
+		return "Hello world!";
+	}
+	
+}
