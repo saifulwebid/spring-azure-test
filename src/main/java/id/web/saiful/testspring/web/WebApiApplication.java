67158b44a5491ed3043e403195fc0ccc1193de80
Muhammad Saiful Islam <muhammad@saiful.web.id>
2017-06-08 15:12:49 +0700

    WebApiApplication extends SpringBootServletInitializer

--- src/main/java/id/web/saiful/testspring/web/WebApiApplication.java
+++ src/main/java/id/web/saiful/testspring/web/WebApiApplication.java
@@ -2,9 +2,16 @@ package id.web.saiful.testspring.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class WebApiApplication {
+public class WebApiApplication extends SpringBootServletInitializer {
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(WebApiApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(WebApiApplication.class, args);
